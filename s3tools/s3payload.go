282efc4328cc555be9879901c007a7747adfb603
Yuriy Sannikov <yury.sannikov@gmail.com>
2016-09-28 18:27:23 -0400

    add mime types & massage cache control

--- s3tools/s3payload.go
+++ s3tools/s3payload.go
@@ -3,7 +3,9 @@ package s3tools
 import (
 	"fmt"
 	"log"
+	"mime"
 	"os"
+	"path"
 
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
@@ -25,14 +27,39 @@ func (payload Payload) process() error {
 
 	uploader := s3manager.NewUploaderWithClient(payload.S3Service)
 
-	_, err := uploader.Upload(&s3manager.UploadInput{
+	uploadInput := s3manager.UploadInput{
 		Body:   file,
 		Bucket: &payload.Bucket,
 		Key:    &payload.FilePath,
-	})
+	}
+	updateMetadata(payload.FilePath, &uploadInput)
+
+	_, err := uploader.Upload(&uploadInput)
 
 	if err != nil {
 		fmt.Printf("Error while processing %s, %s\n", payload.FilePath, err)
 	}
 	return err
+}
+
+func updateMetadata(name string, input *s3manager.UploadInput) {
+	var DefaultCacheControl = "max-age=31536000,public"
+	var HTMLCacheControl = "max-age=86400,public"
+
+	input.CacheControl = &DefaultCacheControl
+
+	fileExt := path.Ext(name)
+
+	if fileExt == ".html" {
+		input.CacheControl = &HTMLCacheControl
+	}
+
+	mimeType := mime.TypeByExtension(fileExt)
+
+	// Fallback to default mime type
+	if len(mimeType) == 0 {
+		mimeType = "application/octet-stream"
+	}
+
+	input.ContentType = &mimeType
 }
